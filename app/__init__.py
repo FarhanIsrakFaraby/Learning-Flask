ebcd5a2bd0bc2b71b78ac87e9f8f1e005e24e48f
FarhanIsrakFaraby <farhanisrak24@gmail.com>
2024-01-28 10:42:28 +0600

    Chapter 11 - Dates and Times

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from config import Config
 from flask_mail import Mail
+from flask_moment import Moment
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,7 +15,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-mail= Mail(app)
+mail = Mail(app)
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
